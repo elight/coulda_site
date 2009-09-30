3715df853eddb30796e195a7f3dda1098301766d
Evan David Light <evan@tiggerpalace.com>
2009-09-29 20:42:46 -0400

    A whole lot more content and updates

--- lib/pre_formatted_text.rb
+++ lib/pre_formatted_text.rb
@@ -22,12 +22,37 @@ HERE
 
 MAIN_PAGE_PLAIN_TEXT_SAMPLE = <<HERE
 Feature: Define a feature
-  In order to write acceptance tests with less code
-  As a developer
-  I want to use a simple internal DSL
+In order to write acceptance tests with less code
+As a developer
+I want to use a simple internal DSL
 
-  Scenario: Demonstrate how coulda works
-    Given a pending prereq
-    When something happens
-    Then expect something else
+Scenario: Demonstrate how coulda works
+  Given a pending prereq
+  When something happens
+  Then expect something else
 HERE
+
+TEST_HELPER = <<HERE
+    require 'rubygems'
+require 'coulda'
+include Coulda
+HERE
+
+PENDING_FEATURE = <<HERE
+    require 'test_helper'
+
+Feature "A pending feature"
+HERE
+
+PENDING_FEATURE_WITH_PURPOSE = <<HERE
+    require 'test_helper'
+
+Feature "A pending feature" do
+  in_order_to "accomplish something else"
+  as_a "user"
+  i_want_to "do something"
+end
+HERE
+
+
+
